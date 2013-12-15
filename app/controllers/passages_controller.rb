429dbbd2653195fd379090adbdab74d1fc03ff08
Caroline Meeks <caroline@meekshome.com>
2013-12-14 22:23:23 -0500

    Working my way through the tutorial

--- app/controllers/passages_controller.rb
+++ app/controllers/passages_controller.rb
@@ -2,4 +2,17 @@ class PassagesController < ApplicationController
 
   def new
   end
+
+  def create
+    @passage = Passage.new(passage_params)
+
+    @passage.save
+    redirect_to @passage
+  end
+  
+  private
+  def passage_params
+    params.require(:passage).permit(:prompt, :content)
+  end
+
 end
--- app/models/passage.rb
+++ app/models/passage.rb
@@ -1,3 +1,3 @@
 class Passage < ActiveRecord::Base
-  has_many: words
+  has_many :words
 end
