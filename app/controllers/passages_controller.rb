20b9f8419801892664e1e65f2be2d2d8f93e49ba
Caroline Meeks <caroline@meekshome.com>
2013-12-17 22:10:38 -0500

    first iteration!

--- app/controllers/passages_controller.rb
+++ app/controllers/passages_controller.rb
@@ -10,7 +10,33 @@ class PassagesController < ApplicationController
 
   def show
     @passage = Passage.find(params[:id])
+
+    @words = Word.where(passage_id: @passage.id)
+    gon.numWords = @words.length    
     @highlight_url = url_for(:only_path => false) + "/highlights"
+
+    #Code for highlighting
+    @heatmap_colors = ["#ffffff", "#ffffcc", "#ffeda0", "#fed976", "#feb24c", "#fd8d3c", "#fc4e2a"]
+
+    #This is a really inefficient way to get the highest highlighted value.
+    n_hl = []
+    @words.each do |w| 
+      n_hl << w.highlights.count
+    end
+    
+    max_hl = n_hl.max
+    min_hl = n_hl.min
+    spread = max_hl - min_hl
+    
+    #Now lets loop through the whole thing again and set up the colors.
+    @text_map = @words.inject([]) do |result, w|
+      #Divide the words into 9 bins. In this case by length but eventually by the number of people who highligted it.
+      level = (((w.highlights.count - min_hl.to_f) / spread) * 6.0).floor
+      color = @heatmap_colors[level]
+      #What I really want to do here is create a data structure where I can use the name rather then remembering which order the elements are in. But that just isn't working for me right now.
+      result << [w.word, color]
+    end
+#    binding.pry
   end
 
   def create
