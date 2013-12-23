0f310d7e87b1190641755d77f39ef3b2b32cd5ea
Caroline Meeks <caroline@meekshome.com>
2013-12-22 21:03:07 -0500

    working on colors and towards a key

--- app/controllers/passages_controller.rb
+++ app/controllers/passages_controller.rb
@@ -15,33 +15,49 @@ class PassagesController < ApplicationController
 
     @words = Word.where(passage_id: @passage.id).order("id")
     gon.numWords = @words.length    
+    
+    #How many students have done any highlights for this passage?
+
     @highlight_url = url_for(:only_path => false) + "/highlights"
 
     #Code for highlighting
-    @heatmap_colors = ["#ffffff", "#ffffcc", "#ffeda0", "#fed976", "#feb24c", "#fd8d3c", "#fc4e2a"]
+    @heatmap_colors = ["#95A3B5", "#41b6c4", "#7fcdbb", "#c7e9b4", "#edf8b1", "#ffffd9"]
+    max_i_colors = (@heatmap_colors.count * 1.0) - 1
 
     #This is a really inefficient way to get the highest highlighted value.
     n_hl = []
+    w_ids = []
+    s_ids = Hash.new
     @words.each do |w| 
       n_hl << w.highlights.count
+      w_ids << w.id
+      
     end
     
-    max_hl = n_hl.max
-    min_hl = n_hl.min
-    spread = max_hl - min_hl
+    @num_students = Highlight.find_all_by_id(w_ids, :group => "session_id").count
 
-    if max_hl >= 1 
+    @max_hl = n_hl.max
+    min_hl = n_hl.min
+    @spread = @max_hl - min_hl
+    
+    if @max_hl >= 1 
       #Now lets loop through the whole thing again and set up the colors.
         @text_map = @words.inject([]) do |result, w|
           #Divide the words into 9 bins. In this case by length but eventually by the number of people who highligted it.
-          level = (((w.highlights.count - min_hl.to_f) / spread) * 6.0).floor
-          color = @heatmap_colors[level]
+        if w.highlights.count == 1
+          #even if there are many highlights we want to show that one person highlighted the word.
+          level = 1
+        else
+          level = (((w.highlights.count - min_hl.to_f) / @spread) * max_i_colors).floor
+        end
+        color = @heatmap_colors[level]
           #What I really want to do here is create a data structure where I can use the name rather then remembering which order the elements are in. But that just isn't working for me right now.
+
           result << [w.word, color]
         end
       else
         @text_map = @words.inject([]) do |result, w|
-          #Divide the words into 9 bins. In this case by length but eventually by the number of people who highligted it.
+        #no highlighting yet.
         color = @heatmap_colors[0]
           #What I really want to do here is create a data structure where I can use the name rather then remembering which order the elements are in. But that just isn't working for me right now.
           result << [w.word, color]
